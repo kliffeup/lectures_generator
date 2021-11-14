f10bbe73c4756a80b2a51cd33febc10ec2603f85
kliffeup <azatvaleev356778@gmail.com>
2021-11-15 02:11:55 +0300

    added text-to-speech part

---
+++ script.py
@@ -0,0 +1,18 @@
+import argparse
+
+from tts import generate_audio
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--file', type=str, required=True,
+                    help='path to a file with texts to synthesize')
+
+parser.add_argument('-w', '--words_to_replace', type=str, required=False,
+                    default='./words_to_replace.json',
+                    help='path to a json file with words to replace')
+
+parser.add_argument('-p', '--pause_dur', type=int, required=False,
+                    default=3,
+                    help='pause duration between two paragraphs')
+
+args = parser.parse_args()
+generate_audio(args.file, args.words_to_replace, args.pause_dur)
---
+++ tts.py
@@ -0,0 +1,65 @@
+import os
+import json
+import glob
+from pydub import AudioSegment
+
+
+
+def get_file_name(input_text_path):
+    return input_text_path.split(sep='/')[-1].split(sep='.')[0]
+
+
+def build_align():
+    os.system('python ./Grad-TTS/model/monotonic_align/setup.py build_ext --inplace')
+
+
+def install_pretrained():
+    os.system('gdown --id 15AeZO2Zo4NBl7PG8oGgfQk0J1PpjaOgI '
+              '-O ./Grad-TTS/checkpts/hifigan.pt')
+    os.system('gdown --id 1YrlswCD2Q_IUlvFtQQ-gnfkG7FEvRoPJ '
+              '-O ./Grad-TTS/checkpts/grad-tts.pt')
+
+
+def replace_words(input_text_path=None, words_to_replace_path=None, output_file=None):
+    with open(input_text_path, 'r', encoding='utf-8') as file:
+        filedata = file.read()
+
+    with open(words_to_replace) as json_file:
+        word_replacement = json.load(json_file)
+        for word, replacement in word_replacement.items():
+            filedata = filedata.replace(word, replacement)
+
+    with open(output_file, 'w', encoding='utf-8') as file:
+        file.write(filedata)
+
+
+def merge_paragraphs(input_file_name: str, pause_dur: int):
+    sound = AudioSegment.from_wav(f'./Grad-TTS/temp/{input_file_name}_0_par.wav')
+    sound.export(f'./Grad-TTS/out/{input_file_name}_full.wav', format='wav')
+    par_count = len(glob.glob1('./Grad-TTS/temp/', '*.wav'))
+
+    for i in range(1, par_count):
+        sound = AudioSegment.from_wav(f'./Grad-TTS/out/{input_file_name}_full.wav')
+        silence = AudioSegment.from_wav(f'./Grad-TTS/pause/{pause_dur}s.wav')
+        par = AudioSegment.from_wav(f'./Grad-TTS/temp/{input_file_name}_{i}_par.wav')
+
+        combined_sounds = sound + silence + par
+        combined_sounds.export(f'./Grad-TTS/out/{input_file_name}_full.wav', format='wav')
+
+
+def generate_audio(input_text_path: str, words_to_replace_path: str, pause_dur: int=3):
+    build_align()
+    if not (os.path.exists('./Grad-TTS/checkpts/hifigan.pt') or
+            os.path.exists('./Grad-TTS/checkpts/grad-tts.pt')):
+        install_pretrained()
+
+    output_text_path = f'./Grad-TTS/temp/{get_file_name(input_text_path)}_fix.txt'
+    replace_words(input_text_path, words_to_replace_path, output_text_path)
+
+    os.system(f'python inference.py -f {output_text_path} -c ./Grad-TTS/checkpts/grad-tts.pt')
+
+
+    merge_paragraphs(get_file_name(input_text_path), pause_dur)
+    os.system('rm -f ./Grad-TTS/temp/*')
+
+    return f'./Grad-TTS/out/{get_file_name(input_text_path)}_full.wav'

32aec9788d8c88f7a013e2e5c6bd49eab41ae858
kliffeup <azatvaleev356778@gmail.com>
2021-11-16 15:17:55 +0300

    set correct paths

--- script.py
+++ script.py
@@ -16,4 +16,7 @@ parser.add_argument('-p', '--pause_dur', type=int, required=False,
                     help='pause duration between two paragraphs')
 
 args = parser.parse_args()
-generate_audio(os.path.abspath(args.file), os.path.abspath(args.words_to_replace), args.pause_dur)
+generate_audio(os.path.join(os.path.abspath('Grad-TTS'), args.file),
+               os.path.join(os.path.abspath('Grad-TTS'),
+                            os.path.abspath(args.words_to_replace)),
+               args.pause_dur)

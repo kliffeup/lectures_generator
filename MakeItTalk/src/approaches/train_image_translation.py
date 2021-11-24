b214daaf8011362ec8af56a293ca651f617210ec
kliffeup <azatvaleev356778@gmail.com>
2021-11-24 12:40:29 +0300

    update video part

--- MakeItTalk/src/approaches/train_image_translation.py
+++ MakeItTalk/src/approaches/train_image_translation.py
@@ -488,7 +488,7 @@ class Image_translation_block():
         if (filename is None):
             filename = 'v'
         os.system(
-            'ffmpeg -loglevel error -y -i out.mp4 -i {} -pix_fmt yuv420p -strict -2 examples/{}_{}.mp4'.format(
+            'ffmpeg -loglevel error -y -i out.mp4 -i {} -pix_fmt yuv420p -strict -2 ./../{}_{}.mp4'.format(
                 'examples/' + filename[9:-16] + '.wav',
                 prefix, filename[:-4]))
         # os.system('rm out.mp4')

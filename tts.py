fb6220701112723d1bf1af539dbde2bf2ffa0eb9
kliffeup <azatvaleev356778@gmail.com>
2021-12-05 21:05:05 +0300

    Updated tts part: added docstrings, updated imports

--- tts.py
+++ tts.py
@@ -1,44 +1,76 @@
-import os
-import json
-import glob
+from glob import glob1
+from json import load
+from os import chdir, system
+from os.path import abspath, exists
+from typing import Any
+
 from pydub import AudioSegment
 
 
-
-def get_file_name(input_text_path):
+def get_file_name(input_text_path: str) -> str:
+    """
+    Extract file name without extension from given path.
+    """
     return input_text_path.split(sep='/')[-1].split(sep='.')[0]
 
 
-def build_align():
-    os.chdir(os.path.abspath('model/monotonic_align'))
-    os.system('python setup.py build_ext --inplace')
-    os.chdir('../..')
+def build_align() -> None:
+    """
+    Build voice alignment.
+    Need for speech synthesis.
+    """
+    chdir(abspath('model/monotonic_align'))
+    system('python setup.py build_ext --inplace')
+    chdir('../..')
 
 
-def install_pretrained():
-    os.system('gdown --id 15AeZO2Zo4NBl7PG8oGgfQk0J1PpjaOgI '
+def install_pretrained() -> None:
+    """
+    Install pretrained model weights.
+    """
+    system('gdown --id 15AeZO2Zo4NBl7PG8oGgfQk0J1PpjaOgI ' +
               '-O ./Grad-TTS/checkpts/hifigan.pt')
-    os.system('gdown --id 1YrlswCD2Q_IUlvFtQQ-gnfkG7FEvRoPJ '
+    system('gdown --id 1YrlswCD2Q_IUlvFtQQ-gnfkG7FEvRoPJ ' +
               '-O ./Grad-TTS/checkpts/grad-tts.pt')
 
 
-def replace_words(input_text_path=None, words_to_replace_path=None, output_file=None):
-    with open(input_text_path, 'r', encoding='utf-8') as file:
-        filedata = file.read()
+def replace_words(
+        input_text_path: Any=None,
+        replacements_file_path: Any=None,
+        output_file_path: Any=None,
+) -> None:
+    """
+    Replace words in input text file with replacements specified in .json file.
+    Hack to have better model pronunciation of separate words. 
 
-    with open(words_to_replace_path) as json_file:
-        word_replacement = json.load(json_file)
-        for word, replacement in word_replacement.items():
-            filedata = filedata.replace(word, replacement)
+    :param input_text_path: path to input text file
+    :param replacements_file_path: path to .json file with pairs 'word: replacement'
+    :param output_file_path: path to output file with replaced words
+    """
 
-    with open(output_file, 'w', encoding='utf-8') as file:
-        file.write(filedata)
+    with open(input_text_path, mode='r', encoding='utf-8') as input_file:
+        input_text = input_file.read()
+
+    if replacements_file_path is not None:
+        with open(replacements_file_path) as replaces_file:
+            word_replacement = load(replaces_file)
+            for word, replacement in word_replacement.items():
+                input_text = input_text.replace(word, replacement)
+
+    with open(output_file_path, mode='w', encoding='utf-8') as output_file:
+        output_file.write(input_text)
 
 
-def merge_paragraphs(input_file_name: str, pause_dur: int):
+def merge_paragraphs(input_file_name: str, pause_dur: int) -> None:
+    """
+    Join audio files which correspond text paragraphs together putting pauses between them.
+
+    :param input_file_name:
+    :param pause_dur: pause duration, possible values: 3, 5
+    """
     sound = AudioSegment.from_wav(f'./temp/{input_file_name}_0_par.wav')
     sound.export(f'./out/{input_file_name}_full.wav', format='wav')
-    par_count = len(glob.glob1('./temp/', '*.wav'))
+    par_count = len(glob1('./temp/', '*.wav'))
 
     for i in range(1, par_count):
         sound = AudioSegment.from_wav(f'./out/{input_file_name}_full.wav')
@@ -49,20 +81,27 @@ def merge_paragraphs(input_file_name: str, pause_dur: int):
         combined_sounds.export(f'./out/{input_file_name}_full.wav', format='wav')
 
 
-def generate_audio(input_text_path: str, words_to_replace_path: str, pause_dur: int=3):
-    os.chdir(os.path.abspath('Grad-TTS/'))
+def generate_speech(input_text_path: str, replacements_path: Any=None, pause_dur: int=3) -> None:
+    """
+    Generate audio by given input text file.
+
+    :param input_text_path: path to input text file
+    :param replacements_path: path to .json file with pairs 'word: replacement'
+    :param pause_dur: duration between paragraphs, possible values: 3, 5
+    :return: path to output .wav file with synthesised speech
+    """
+    chdir(abspath('Grad-TTS/'))
     build_align()
-    if not (os.path.exists('./checkpts/hifigan.pt') or
-            os.path.exists('./checkpts/grad-tts.pt')):
+    if not (exists('./checkpts/hifigan.pt') or exists('./checkpts/grad-tts.pt')):
         install_pretrained()
 
     output_text_path = f'./temp/{get_file_name(input_text_path)}_fix.txt'
-    replace_words(input_text_path, words_to_replace_path, output_text_path)
+    replace_words(input_text_path, replacements_path, output_text_path)
 
-    os.system(f'python ./inference.py -f {output_text_path} -c ./checkpts/grad-tts.pt')
+    system(f'python ./inference.py -f {output_text_path} -c ./checkpts/grad-tts.pt')
 
 
     merge_paragraphs(get_file_name(input_text_path), pause_dur)
-    os.system('rm -f ./temp/*')
-    os.chdir('..')
+    system('rm -f ./temp/*')
+    chdir('..')
     return f'./Grad-TTS/out/{get_file_name(input_text_path)}_full.wav'
